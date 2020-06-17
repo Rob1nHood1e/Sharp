d471a3277eefa6a309e55afebbcfdf45d4a8519e
RobinHioodie <vladyugay64@gmail.com>
2020-06-17 15:40:08 +0300

    Вооот

---
+++ 1.2/1.2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Hamiltonian_path__ind_1._2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 458);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(445, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запуск программы";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(102, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Матрица смежности вершин графа:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 512);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Гамильтонов путь:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(284, 555);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 21);
+            this.label3.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 53);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(445, 401);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 543);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ 1.2/1.2/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Hamiltonian_path__ind_1._2_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximumSize = new System.Drawing.Size(550, 550);
+            this.WindowState = FormWindowState.Maximized;
+        }
+        public int[,] vertexMatrix;
+        public bool[] usedVertex;
+        public int matrixSize;
+        public List<int> path;
+
+        public bool HamiltonPathExistence(int vertex)
+        {
+            path.Add(vertex);
+
+            if (path.Count == matrixSize)
+            {
+                return true;
+            }
+            usedVertex[vertex] = true;
+            for (int i = 0; i < matrixSize; i++)
+            {
+                if (!usedVertex[i] && vertexMatrix[vertex,i] == 1)
+                {
+                    if (HamiltonPathExistence(i))
+                    {
+                        return true;
+                    }
+                }
+            }
+            path.RemoveAt(path.Count - 1);
+            usedVertex[vertex] = false;
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int t = 0;
+            if (richTextBox1.Text.Equals(""))
+            {
+                StreamReader ifstream = new StreamReader("matrix.txt", System.Text.Encoding.Default);
+                richTextBox1.Text += ifstream.ReadToEnd();
+                ifstream.Close();
+            }
+
+            while (!richTextBox1.Text[t++].Equals('\n'))
+            {
+                matrixSize++;
+            }
+
+            vertexMatrix = new int[matrixSize, matrixSize];
+            usedVertex = new bool[matrixSize];
+            path = new List<int>(matrixSize);
+            t = 0;
+        
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    vertexMatrix[i, j] = (int)Char.GetNumericValue(richTextBox1.Text[t++]);
+                }
+                t++;
+            }
+
+            StreamWriter ffstream = new StreamWriter("result.txt", false, System.Text.Encoding.Default);
+            bool flag = false;
+            for (int i = 0; i < matrixSize && !flag; i++)
+            {
+                if (HamiltonPathExistence(i))
+                {
+                    foreach (int item in path)
+                    {
+                        ffstream.Write(item);
+                        label3.Text += Convert.ToString(item);
+
+                        if (!item.Equals(path.Last()))
+                        {
+                            ffstream.Write("-");
+                            label3.Text += "-";
+                        }
+                    }
+                    flag = true;
+                }
+            }
+
+            if (!flag)
+            {
+                label3.Text += "Гамильтонова пути не существует.";
+                ffstream.Write("Гамильтонова пути не существует.");
+            }
+            ffstream.Close();
+        }
+    }
+}
